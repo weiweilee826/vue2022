6e3c1f464472cbc2977afba9f172c5cc53e5471c
weiwei <weiweilee826@gmail.com>
2022-09-15 16:54:18 +0800

    bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
 
-createApp(App).use(router).mount('#app')
+import App from "./App.vue";
+import router from "./router";
+
+import BootstrapVue3 from 'bootstrap-vue-3';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
+createApp(App)
+  .use(router)
+  .use(BootstrapVue3)
+  .mount("#app")
+

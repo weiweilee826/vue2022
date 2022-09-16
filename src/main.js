b0c17fdfb31f62746a535fafd7578e2d3890deb6
weiwei <weiweilee826@gmail.com>
2022-09-16 17:11:07 +0800

    安裝axios

--- src/main.js
+++ src/main.js
@@ -7,8 +7,12 @@ import BootstrapVue3 from 'bootstrap-vue-3';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 createApp(App)
   .use(router)
   .use(BootstrapVue3)
-  .mount("#app")
+  .use(VueAxios, axios)
+  .mount("#app");
 

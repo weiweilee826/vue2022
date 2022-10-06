8311d80822ed47d8030876df28fa70c0cb0f07ed
weiwei <weiweilee826@gmail.com>
2022-10-06 16:45:30 +0800

    新增分頁功能

---
+++ src/components/MultiplePages.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item" :class="{ disable: !pagination.has_pre }">
+          <a
+            class="page-link"
+            href="#"
+            aria-label="Previous"
+            @click.prevent="emitGetProduct(pagination.current_page - 1)"
+          >
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li
+          class="page-item"
+          v-for="page in pagination.total_pages"
+          :key="page"
+          :class="{ active: pagination.current_page === page }"
+        >
+          <a class="page-link" href="#" @click.prevent="emitGetProduct(page)">{{
+            page
+          }}</a>
+        </li>
+        <li
+          class="page-item"
+          :class="{ disable: !pagination.has_next }"
+          @click.prevent="emitGetProduct(pagination.current_page + 1)"
+        >
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pagination"],
+  data() {
+    return {};
+  },
+  methods: {
+    emitGetProduct(page) {
+      this.$emit("getProducts", page);
+    },
+  },
+};
+</script>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -39,6 +39,47 @@
       </tbody>
     </table>
 
+    <!-- 分頁 -->
+    <Pagination
+      :pagination="pagination"
+      @getProducts="getProducts"
+    ></Pagination>
+
+    <!-- <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item" :class="{ disabled: !pagination.has_pre }">
+          <a
+            class="page-link" 
+            href="#"
+            aria-label="Previous"
+            @click.prevent="getProducts(pagination.current_page - 1)"
+          >
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li
+          class="page-item"
+          v-for="page in pagination.total_pages"
+          :key="page"
+          :class="{ active: pagination.current_page === page }"
+        >
+          <a class="page-link" href="#" @click.prevent="getProducts(page)">
+            {{ page }}
+          </a>
+        </li>
+        <li class="page-item" :class="{ disabled: !pagination.has_next }">
+          <a
+            class="page-link"
+            href="#"
+            aria-label="Next"
+            @click.prevent="getProducts(pagination.current_page + 1)"
+          >
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav> -->
+
     <!-- 新增商品modal -->
     <div
       class="modal fade"
@@ -261,11 +302,16 @@
 
 <script>
 import { Modal } from "bootstrap";
+import Pagination from "@/components/MultiplePages.vue";
 
 export default {
+  components: {
+    Pagination,
+  },
   data() {
     return {
       products: [],
+      pagination: {},
       tempProduct: {},
       myModal: {},
       isNew: false,
@@ -276,13 +322,14 @@ export default {
     };
   },
   methods: {
-    getProducts() {
-      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_CUSTOM_PATH}/products`;
+    getProducts(page = 1) {
+      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_CUSTOM_PATH}/products?page=${page}`;
       const vm = this;
       vm.isLoading = true;
       this.$http.get(api).then((response) => {
         vm.isLoading = false;
         vm.products = response.data.products;
+        vm.pagination = response.data.pagination;
       });
     },
     openModal(isNew, item) {

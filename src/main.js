e8a57ea12f4dc50f80b9e3b60b1768350aae988f
weiwei <weiweilee826@gmail.com>
2022-10-07 16:17:03 +0800

    新增價格filter

--- src/main.js
+++ src/main.js
@@ -10,37 +10,44 @@ import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
-import VueLoading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/vue-loading.css';
+import VueLoading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-library.add(faSpinner)
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+library.add(faSpinner);
 
 import mitt from "mitt";
 
 const app = createApp(App);
-  app.use(router)
+app
+  .use(router)
   .use(BootstrapVue3)
   .use(VueAxios, axios)
-  .provide('axios', app.config.globalProperties.axios)
+  .provide("axios", app.config.globalProperties.axios)
   // .use(VueLoading)
-  .component('Loading',VueLoading)
-  .component('font-awesome-icon', FontAwesomeIcon)
+  .component("Loading", VueLoading)
+  .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
 
-  router.beforeEach((to, from) => {
-    console.log("to", to, "from", from);
-    if (to.meta.requiresAuth) {
-      const api = `${process.env.VUE_APP_API}/api/user/check`;
-      axios.post(api).then((response) => {
-        if (!response.data.success) {
-          return { name: "login" };
-        }
-      });
-    }
-  });
+app.config.globalProperties.$filters = {
+  currencyUSD(value) {
+    return "$" + value;
+  },
+};
+
+router.beforeEach((to, from) => {
+  console.log("to", to, "from", from);
+  if (to.meta.requiresAuth) {
+    const api = `${process.env.VUE_APP_API}/api/user/check`;
+    axios.post(api).then((response) => {
+      if (!response.data.success) {
+        return { name: "login" };
+      }
+    });
+  }
+});
 axios.defaults.withCredentials = true;
 
 app.config.globalProperties.$mybus = mitt();
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -21,8 +21,10 @@
         <tr v-for="item in products" :key="item.id">
           <td>{{ item.category }}</td>
           <td>{{ item.title }}</td>
-          <td class="text-end">{{ item.origin_price }}</td>
-          <td class="text-end">{{ item.price }}</td>
+          <td class="text-end">
+            {{ $filters.currencyUSD(item.origin_price) }}
+          </td>
+          <td class="text-end">{{ $filters.currencyUSD(item.price) }}</td>
           <td>
             <span v-if="item.is_enabled" class="text-success">啟用</span>
             <span v-else>未啟用</span>

ea107b76dbd3bf3b4dcc6b09693b6c8bcb2e892f
weiwei <weiweilee826@gmail.com>
2022-10-28 17:03:24 +0800

    新增商品 modal

--- src/views/CustomerOrder.vue
+++ src/views/CustomerOrder.vue
@@ -59,19 +59,94 @@
         </div>
       </div>
     </div>
+
+    <!-- 商品modal -->
+    <div
+      class="modal fade"
+      id="productModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">
+              {{ product.title }}
+            </h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img :src="product.image" class="img-fluid" alt="" />
+            <blockquote class="blockquote mt-3">
+              <p class="mb-0">{{ product.content }}</p>
+              <footer class="blockquote-footer text-right">
+                {{ product.description }}
+              </footer>
+            </blockquote>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h4" v-if="!product.price">
+                {{ product.origin_price }} 元
+              </div>
+              <del class="h6" v-if="product.price"
+                >原價 {{ product.origin_price }} 元</del
+              >
+              <div class="h4" v-if="product.price">
+                現在只要 {{ product.price }} 元
+              </div>
+            </div>
+            <select name="" class="form-control mt-3" v-model="product.num">
+              <option :value="num" v-for="num in 10" :key="num">
+                選購 {{ num }} {{ product.unit }}
+              </option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-muted text-nowrap mr-3">
+              小計 <strong>{{ product.num * product.price }}</strong> 元
+            </div>
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="addtoCart(product.id, product.num)"
+            >
+              <i
+                class="fas fa-spinner fa-spin"
+                v-if="product.id === status.loadingItem"
+              ></i>
+              加到購物車
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  
   </div>
 </template>
 
 <script>
+import { Modal } from "bootstrap";
+
 export default {
   data() {
     return {
       products: [],
       product: {},
+      myModal: {},
       status: {
         loadingItem: "",
       },
       isLoading: false,
+      carts: [],
     };
   },
   methods: {
@@ -90,12 +165,42 @@ export default {
       vm.status.loadingItem = id;
       this.$http.get(url).then((response) => {
         vm.product = response.data.product;
+        vm.myModal.show();
         vm.status.loadingItem = "";
       });
     },
-  },
-  created() {
-    this.getProducts();
-  },
+    addtoCart(id, qty = 1) {
+      const vm = this;
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_CUSTOM_PATH}/cart`;
+      vm.status.loadingItem = id;
+      const cart = {
+        product_id: id,
+        qty,
+      };
+      this.$http.post(url, { data: cart }).then((response) => {
+        console.log("x", response);
+        vm.status.loadingItem = "";
+        vm.getCart();
+        this.myModal.hide();
+      });
+    },
+    getCart() {
+      const vm = this;
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_CUSTOM_PATH}/cart`;
+      vm.isLoading = true;
+      this.$http.get(url).then((response) => {
+        vm.carts = response.data.data.carts;
+        vm.isLoading = false;
+      });
+    },
+   },
+    mounted() {
+      this.myModal = new Modal("#productModal", {});
+    },
+    created() {
+      this.getProducts();
+      this.getCart();
+    },
+  
 };
 </script>

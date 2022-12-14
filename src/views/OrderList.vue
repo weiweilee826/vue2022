25ee82f0658aae01cbd3005567fcf93c53e0b4ad
weiwei <weiweilee826@gmail.com>
2022-12-14 11:21:51 +0800

    新增訂單列表

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -11,17 +11,19 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in orders" :key="item.id">
-          <td></td>
+        <tr v-for="order in orders" :key="order.id">
+          <td>{{ order.create_at }}</td>
+          <td>{{ order.email }}</td>
+          <td v-html="order.product"></td>
+          <td>{{ order.total }}</td>
+          <td v-if="order.is_paid">已付款</td>
+          <td v-else>未付款</td>
         </tr>
       </tbody>
     </table>
 
     <!-- 分頁 -->
-    <Pagination
-      :pagination="pagination"
-      @getProducts="getOrders"
-    ></Pagination>
+    <Pagination :pagination="pagination" @getProducts="getOrders"></Pagination>
   </div>
 </template>
 
@@ -34,9 +36,10 @@ export default {
   },
   data() {
     return {
+      products: "",
       isLoading: false,
       orders: [],
-      pagination:{},
+      pagination: {},
     };
   },
   methods: {
@@ -45,8 +48,21 @@ export default {
       const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_CUSTOM_PATH}/orders`;
       vm.isLoading = true;
       this.$http.get(url).then((response) => {
-        console.log("ooo", response);
-        vm.orders = response.data.orders;
+        for (let order of response.data.orders) {
+          let myorder = {};
+          myorder.id = order.id;
+          myorder.create_at = order.create_at;
+          myorder.email = order.user.email;
+          myorder.is_paid = order.is_paid;
+          myorder.total = order.total;
+
+          let str = "";
+          for (let product of Object.values(order.products)) {
+            str += product.product.title + " 數量: " + product.qty + "<br>";
+          }
+          myorder.product = str;
+          this.orders.push(myorder);
+        }
         vm.isLoading = false;
         vm.pagination = response.data.pagination;
       });
